622df8f4c60eef98effb8389278b2a7f516ab3f4
acup1 <georgiyegoriy@gmail.com>
2025-09-19 00:26:44 +0300

    upd

---
+++ lua/plugins/codium.lua
@@ -0,0 +1,11 @@
+{
+    "Exafunction/windsurf.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "hrsh7th/nvim-cmp",
+    },
+    config = function()
+        require("codeium").setup({
+        })
+    end
+},
