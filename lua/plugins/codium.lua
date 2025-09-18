d5717f222c140b8a86f71c7538d3bb2a1f254e9e
acup1 <georgiyegoriy@gmail.com>
2025-09-19 00:37:10 +0300

    suka blyat

--- lua/plugins/codium.lua
+++ lua/plugins/codium.lua
@@ -1,8 +1,14 @@
+---@type LazySpec
 return {
-  "Exafunction/windsurf.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "hrsh7th/nvim-cmp",
+  {
+    "Exafunction/windsurf.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "hrsh7th/nvim-cmp",
+    },
+    config = function() require("codeium").setup {} end,
   },
-  config = function() require("codeium").setup {} end,
 }
+
+-- Failed to load `plugins.codium`:
+-- /home/acup/.config/nvim/lua/plugins/codium.lua:1: unexpected symbol near '{'
